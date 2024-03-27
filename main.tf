87ba8063aec2d69cd7d2ebbe98023a92200750d7
Tata94C <123743925+Tata94C@users.noreply.github.com>
2024-03-27 17:14:02 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -11,3 +11,37 @@ terraform {
 provider "azurerm" {
   features {}
 }
+
+resource "azurerm_resource_group" "my_resource_group" {
+  name     = "rg-${var.nom_prenom}-${random_integer.unique_id.result}"
+  location = "West Europe"
+}
+
+resource "azurerm_app_service_plan" "my_app_service_plan" {
+  name                = "asp-${var.nom_prenom}-${random_integer.unique_id.result}"
+  location            = azurerm_resource_group.my_resource_group.location
+  resource_group_name = azurerm_resource_group.my_resource_group.name
+  sku {
+    tier = "Basic"
+    size = "B1"
+  }
+}
+
+resource "azurerm_linux_web_app" "my_web_app" {
+  name                = "webapp-${var.nom_prenom}-${random_integer.unique_id.result}"
+  location            = azurerm_resource_group.my_resource_group.location
+  resource_group_name = azurerm_resource_group.my_resource_group.name
+
+  site_config {
+    java_version           = "1.8"
+    java_container         = "TOMCAT"
+    java_container_version = "9.0"
+  }
+}
+
+variable "nom_prenom" {
+  description = "Nom et prénom pour générer les noms des ressources"
+}
+
+variable "random_integer" {
+  description 
